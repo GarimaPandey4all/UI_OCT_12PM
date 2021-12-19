9166eda8c565924e58168ce45770129a46deb065
Garima Pandey <59961721+GarimaPandey4all@users.noreply.github.com>
2021-12-19 16:55:42 +0530

    Add files via upload

---
+++ Javascript/BasicSyntax/BasicJS.js
@@ -0,0 +1,6 @@
+function showName()
+{
+    username = document.getElementById("box_1").value;
+    // document.getElementById("result").innerHTML += username;
+    document.getElementById("result").innerHTML = username;
+}
